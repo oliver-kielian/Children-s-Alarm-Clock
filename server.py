28ec9943e47bb2d80a024d5bee265b03b3b421ed
Oliver Kielian <oliverkielian02@gmail.com>
2025-02-28 13:49:54 -0700

    Created basic flask app setup

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+#import serial 
+
+app = Flask(__name__)
+app.config["SECERT_KEY"] = "mycoolawesomekey"
+#ser = serial.Serial("COM6", 9600)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('indx.html')
+
+
+@app.route('/morningLight', methods=['POST'])
+def turnOnMLight():
+    color = request.form.get('morningColor')
+    print("turning on morning light as %s", color)
+    #ser.write(b'H') #I have no clue what this is doing, trying stuff out
+
+@app.route('/nightLight', methods=['POST'])
+def turnOnNLight():
+    color = request.form.get('nightColor')
+    print("turning on night light %s", color)
+    #ser.write(b'H') #I have no clue what this is doing, trying stuff out
+
+if __name__ == '__main__':
+     app.run(debug=True)

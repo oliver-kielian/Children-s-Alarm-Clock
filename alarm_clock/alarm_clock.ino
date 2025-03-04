1faf6d5c5bd7891b832fecedb7b677102cd39bbf
Oliver Kielian <oliverkielian02@gmail.com>
2025-03-04 14:14:09 -0700

    added serial commications + light code

--- alarm_clock/alarm_clock.ino
+++ alarm_clock/alarm_clock.ino
@@ -1,9 +1,32 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 6
+#define NUM_PINS 64
+
+Adafruit_NeoPixel matrix(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  //https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
+  matrix.begin();
 
+  //https://forum.arduino.cc/t/code-optimising-ws2812-led-matrix-arduino/1068713
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(Serial.available() > 0)
+  {
+    int r, g, b;
+    String data = Serial.readStringUntil("\n");
 
+    if(sscanf(data.c_str(), "%d,%d,%d", &r, &g, &b) == 3){
+      for (int i = 0; i < NUM_LEDS; i++) {
+        matrix.setPixelColor(i, matrix.Color(r, g, b));
+        Serial.println("martix %d on", i);
+      }
+      matrix.show();
+    }
+  }
 }
--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
-#import serial 
+import serial 
+import serial.tools.list_ports
+
+arduino_ports = [
+    p.device
+    for p in serial.tools.list_ports.comports()
+    if 'Arduino' in p.description 
+]
+
+#https://stackoverflow.com/questions/24214643/python-to-automatically-select-serial-ports-for-arduino
 
 app = Flask(__name__)
 app.config["SECERT_KEY"] = "mycoolawesomekey"
-#ser = serial.Serial("COM6", 9600)
+ser = serial.Serial(arduino_ports[0], 9600)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,15 +22,19 @@ def index():
 
 @app.route('/morningLight', methods=['POST'])
 def turnOnMLight():
-    color = request.form.get('morningColor')
-    return ("turning on morning light as %s", color)
-    #ser.write(b'H') #I have no clue what this is doing, trying stuff out
+    color= request.json.get("morningColor", "#FFFFFF")
+    color= color.lstrip("#")
+    r, g, b = (int(color[i:i+2], 16) for i in (0, 2, 4))
+    ser.write(f"{r},{g},{b}\n".encode())
+    return f"Turing on Morning Light as {color}"
 
 @app.route('/nightLight', methods=['POST'])
 def turnOnNLight():
-    color = request.form.get('nightColor')
-    print("turning on night light %s", color)
-    #ser.write(b'H') #I have no clue what this is doing, trying stuff out
+    color= request.json.get("morningColor", "#FFFFFF")
+    color= color.lstrip("#")
+    r, g, b = (int(color[i:i+2], 16) for i in (0, 2, 4))
+    ser.write(f"{r},{g},{b}\n".encode())
+    return f"Turing on Night Light as {color}"
 
 if __name__ == '__main__':
      app.run(debug=True)

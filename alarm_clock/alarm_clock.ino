ae86da5017e636e043cc9583cf73e28259c02b4b
Larry-Lozoya <167731449+Larry-Lozoya@users.noreply.github.com>
2025-03-07 01:46:46 -0700

    Got the Bluetooth to Connect
    Issues: Event Loop needs to close after being done.
    Need to work on States now on arduino for the colors and time.

--- alarm_clock/alarm_clock.ino
+++ alarm_clock/alarm_clock.ino
@@ -1,21 +1,42 @@
 #include <ArduinoBLE.h>
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 6
+#define NUM_PINS 64
 
  // Bluetooth® Low Energy Battery Service
-BLEService batteryService("180F");
+// BLEService batteryService("180F");
 
-// Bluetooth® Low Energy Battery Level Characteristic
-BLEUnsignedCharCharacteristic batteryLevelChar("2A19",  // standard 16-bit characteristic UUID
-    BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
+// // Bluetooth® Low Energy Battery Level Characteristic
+// BLEUnsignedCharCharacteristic batteryLevelChar("2A19",  // standard 16-bit characteristic UUID
+//     BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
+
+
+// set the size of the incoming and outgoing strings. Max 512:
+const int characteristicSize = 128;
+
+// create service and characteristics:
+// This is our unique services and characteristic
+BLEService stringService("c4d6aa1d-e90b-42fa-977e-039e7b401994");
+BLEStringCharacteristic txCharacteristic("c4d6aa1d-e90b-42fa-977e-039e7b401994",
+    BLEWrite | BLERead | BLENotify, characteristicSize);
+
+
+Adafruit_NeoPixel matrix(NUM_PINS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 int oldBatteryLevel = 0;  // last battery level reading from analog input
 long previousMillis = 0;  // last time the battery level was checked, in ms
 
 void setup() {
-  Serial.begin(9600);    // initialize serial communication
-  while (!Serial);
+  Serial.begin(115200);    // initialize serial communication
+  unsigned long start = millis();
+
 
   pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
 
+  // put your setup code here, to run once:
+  //https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
+  matrix.begin();
   // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
@@ -28,17 +49,18 @@ void setup() {
      and can be used by remote devices to identify this Bluetooth® Low Energy device
      The name can be changed but maybe be truncated based on space left in advertisement packet
   */
-  BLE.setLocalName("MyArduino");
-  BLE.setAdvertisedService(batteryService); // add the service UUID
-  // UUID: Universally Unique Identifier
-  batteryService.addCharacteristic(batteryLevelChar); // add the battery level characteristic
-  BLE.addService(batteryService); // Add the battery service
-  batteryLevelChar.writeValue(oldBatteryLevel); // set initial value for this characteristic
+  BLE.setLocalName("Arduino");
+  // // UUID: Universally Unique Identifier
+  // BLE.setAdvertisedService(stringService); // This is the service we are trying to provide so we have no conflicts with existing services
+  // BLE.addCharacteristic(txCharacteristic); // for example what we want to read/write about like writing "B" to serial
+  // BLE.addService(stringService); // Here we are just adding our service to we are able to use it
+  BLE.setAdvertisedService(stringService);
+  stringService.addCharacteristic(txCharacteristic);
+  BLE.addService(stringService);
 
   /* Start advertising Bluetooth® Low Energy.  It will start continuously transmitting Bluetooth® Low Energy
      advertising packets and will be visible to remote Bluetooth® Low Energy central devices
      until it receives a new connection */
-
   // start advertising
   BLE.advertise();
 
@@ -49,7 +71,7 @@ void loop() {
   // wait for a Bluetooth® Low Energy central
   BLEDevice central = BLE.central();
 
-  // Later I assume that this should be a state where we are going into this state when we are being connected to 
+  // Later I assume that this should be a "state" where we are going into this state when we are being connected to 
   // Then if we are not being connnected to that we are still keeping on track with our timers and our lights going off appropriatly at those times
   // For now I think this is good for sprint one and later we can add various states
 
@@ -59,21 +81,36 @@ void loop() {
     // print the central's BT address:
     Serial.println(central.address());
     // turn on the LED to indicate the connection:
-    digitalWrite(LED_BUILTIN, HIGH);
+    // digitalWrite(LED_BUILTIN, HIGH);
 
-    // check the battery level every 200ms
+    //https://forum.arduino.cc/t/code-optimising-ws2812-led-matrix-arduino/1068713
+
+    // put your main code here, to run repeatedly:
+    // if(Serial.available() > 0){
+    //   int r, g, b;
+    //   String data = Serial.readStringUntil('\n');
+
+    //   if(sscanf(data.c_str(), "%d,%d,%d", &r, &g, &b) == 3){
+    //     Serial.println("We did get here from Website!!!");
+    //     digitalWrite(LED_BUILTIN, HIGH);
+    //     for (int i = 0; i < NUM_PINS; i++) {
+    //       matrix.setPixelColor(i, matrix.Color(r, g, b));
+    //     }
+    //     matrix.show();
+    //   }
+    // }
+
     // while the central is connected:
     // Instead of checking for battery level we want to see if we are getting any messages from the website
     // To then later grab that information of the colors to then change but 
     // for now this is a good way to check battery for just a general as well as for later if we decide to go wireless
+    // So while we are connected
     while (central.connected()) {
-      long currentMillis = millis();
-      // if 200ms have passed, check the battery level:
-      if (currentMillis - previousMillis >= 200 && (Serial.available() > 0)) {
-        char incommingMsg = Serial.read();
-        if(incommingMsg == 'H'){
-          previousMillis = currentMillis;
-          updateBatteryLevel();
+      if (txCharacteristic.written()) {
+        String incomingMsg = txCharacteristic.value();
+        if (incomingMsg == "B") {
+          Serial.println("Received The Letter: " + incomingMsg + " from server");
+          digitalWrite(LED_BUILTIN, HIGH);
         }
       }
     }
@@ -87,7 +124,7 @@ void loop() {
     // If we read somthing from the serial like color hue -> Red to Orange
     // Make a Change in the matrix colors
 
-    // If We reasom somthing from the serial like change Date to -> 8:00 - 9:00
+    // If We reason somthing from the serial like change Date to -> 8:00 - 9:00
     // Make a change in the timer for when the colors should activate
     // }
     // digitalWrite(LED_BUILTIN, LOW);
@@ -106,11 +143,12 @@ void updateBatteryLevel() {
   if (batteryLevel != oldBatteryLevel) {      // if the battery level has changed
     Serial.print("Battery Level % is now: "); // print it
     Serial.println(batteryLevel);
-    batteryLevelChar.writeValue(batteryLevel);  // and update the battery level characteristic
+    // batteryLevelChar.writeValue(batteryLevel);  // and update the battery level characteristic
     oldBatteryLevel = batteryLevel;           // save the level for next comparison
   }
 }
 
+<<<<<<< Updated upstream
 // This is a function we will use later to allow for us to read in the serial
 // then make changes to our lights based on what the serial tells us
 void updateNightLightsColor(){
@@ -120,17 +158,15 @@ void updateNightLightsColor(){
 void updateDayTimeLightsColor(){
 
 }
+=======
+
+void updateDayTimeLightsColor(){ }
+>>>>>>> Stashed changes
 
 // Update the timers from what the website is giving us
-void updateTimerForNightLights(){
+void updateTimerForNightLights(){ }
 
-}
+void updateTimerForDayTimeLights(){ }
 
-void updateTimerForDayTimeLights(){
-
-}
-
-void keepTime(){
-
-}
+void keepTime(){ }
 
--- server.py
+++ server.py
@@ -1,9 +1,42 @@
+import asyncio
 from flask import Flask, render_template, request
+<<<<<<< Updated upstream
 import serial 
 
 app = Flask(__name__)
 app.config["SECERT_KEY"] = "mycoolawesomekey"
 # ser = serial.Serial("Aduino", 9200)
+=======
+from bleak import BleakClient
+from bleak import discover
+
+characteristic ='c4d6aa1d-e90b-42fa-977e-039e7b401994'
+'''
+************************************
+This is Stuff for arduino
+'''
+# https://panamahitek.com/en/wireless-communication-between-arduino-and-python-using-ble/
+# Asynchronous function to scan for BLE devices
+async def scan():
+    # Use the discover function from the bleak library to scan for devices
+    devices = await discover()
+
+    # Loop through the list of discovered devices
+    for d in devices:
+        if(d.name and "Arduino" in d.name):
+            print(d)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(scan())
+
+'''
+************************************
+'''
+
+
+app = Flask(__name__)
+app.config["SECERT_KEY"] = "mycoolawesomekey"
+DEVICE_NAME = 'Arduino'
+>>>>>>> Stashed changes
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,13 +44,22 @@ def index():
 
 @app.route('/morningLight', methods=['POST'])
 def turnOnMLight():
+<<<<<<< Updated upstream
     color = request.form.get('morningColor')
     print("turning on morning light as %s", color)
     #ser.write(b'H') #I have no clue what this is doing, trying stuff out
+=======
+    color= request.form.get("morningColor", "#FFFFFF")
+    color= color.lstrip("#")
+    r, g, b = (int(color[i:i+2], 16) for i in (0, 2, 4))
+    # ser.write(f"{r},{g},{b}\n".encode())
+    return f"Turing on Morning Light as {color}"
+>>>>>>> Stashed changes
 
 @app.route('/nightLight', methods=['POST'])
 def turnOnNLight():
     color = request.form.get('nightColor')
+<<<<<<< Updated upstream
     print("turning on night light %s", color)
     #ser.write(b'H') #I have no clue what this is doing, trying stuff out
     ser.write(b'H')
@@ -31,6 +73,30 @@ def getBlutooth():
     print("We Have Recieved The Device Name: ", device_name)
     # Do this later and make serial have the device name
     return '', 204
+=======
+    color= request.form.get("morningColor", "#FFFFFF")
+    color= color.lstrip("#")
+    r, g, b = (int(color[i:i+2], 16) for i in (0, 2, 4))
+    # ser.write(f"{r},{g},{b}\n".encode())
+    return f"Turing on Night Light as {color}"
+    
+@app.route('/bluetooth', methods=['POST'])
+def getBlutooth():
+    address = "9F83D883-81AA-5450-5C2A-5F7A1C9B5E4A"
+    data = "B"
+    ''' https://stackoverflow.com/questions/61543406/asyncio-run-runtimeerror-event-loop-is-closed '''
+    # Come back to this as this session seems to not be closing and needs work on!!!
+    asyncio.set_event_loop(loop)
+    return " ", 200
+
+async def main(address, data):
+    client = BleakClient(address)
+    try:
+        await client.connect()
+        await client.write_gatt_char(characteristic, data.encode('utf-8'))
+    except Exception as e:
+        print(e)
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     # ssl_context=('cert.pem', 'key.pem')

4abed24625e731e46b4603faca4b1e208a6278f7
Larry-Lozoya <167731449+Larry-Lozoya@users.noreply.github.com>
2025-04-28 18:42:02 -0600

    Fully updated ino file
    read description for more details

--- alarm_clock/alarm_clock.ino
+++ alarm_clock/alarm_clock.ino
@@ -17,10 +17,16 @@
 // NeoPixel matrix
 #define LED_PIN 6
 #define NUM_LEDS 64
-uint8_t ledBrightness = 1; // LED matrix brightness 0-255
+uint8_t ledBrightness = 255; // LED matrix brightness 0-255
 int morningAlarmTime = 0;
 int nightAlarmTime = 0;
 int windDownAlarmTime = 0;
+char charbufAM[7];
+char charbufPM[7];
+
+long int rbg_NightTime;
+long int rbg_MorningTime;
+
 DateTime now;
 String AM_Time_Hour;
 String AM_Time_Minute;
@@ -28,6 +34,7 @@ String PM_Time_Hour;
 String PM_Time_Minute;
 String AM_Color;
 String PM_Color;
+
 
 // LCD screen declaration
 Adafruit_ILI9341 LCD = Adafruit_ILI9341(LCD_CS, LCD_DC, LCD_RST);
@@ -72,17 +79,18 @@ enum myState {
   // currentState == NOTCONNECTED
   NIGHTLIGHT,
   MORNINGLIGHT,
-  WINDDOWNLIGHT,
-  // NIGHT ALARM IS FOR THE WIND DOWN LIGHT
-  NIGHTALARM,
-  MORNINGALARM
 };
 enum myState currentState = NOTCONNECTED;
 
 //timing stuff
 unsigned long previousMillis = 0;
-const long interval = 1000; // 1-second update for clock
-const long nightLightInterval = 
+unsigned long nightStart = 0;
+unsigned long morningStart = 0;
+bool nightLightDone = false;
+bool morningLightDone = false;
+const long interval = 1000;
+
+
 
 
 void setup() {
@@ -92,13 +100,9 @@ void setup() {
   // Initialize LCD
   LCD.begin();
   LCD.setRotation(1);
-  LCD.fillScreen(ILI9341_BLACK);
-  LCD.setTextColor(ILI9341_MAGENTA);
-  LCD.setTextSize(5);
-  LCD.setCursor(100, 40);
-  LCD.println(".  .");
-  LCD.setCursor(85, 80);
-  LCD.println("\\___/");
+  LCD.setTextColor(ILI9341_BLACK);
+  snorLaxFace();
+  matrix.begin();
 
   // Initialize RTC
   if (!rtc.begin()) {
@@ -108,11 +112,6 @@ void setup() {
   if (rtc.lostPower()) {
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Set to compile time
   }
-
-  //start matrix
-  //https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
-  
-  //end matrix test for demo
 
   // begin initialization
   if (!BLE.begin()) {
@@ -149,11 +148,11 @@ void loop() {
         currentState = CONNECTED;
       }
       if(AM_Time_Hour == String(now.hour()) && AM_Time_Minute == String(now.minute())){
-        Serial.println("Our Time is good");
-        currentState = MORNINGALARM;
+        currentState = MORNINGLIGHT;
       }
       if(PM_Time_Hour == String(now.hour()) && PM_Time_Minute == String(now.minute())){
-        currentState = NIGHTALARM;
+        Serial.println("Connected to NIGHTLIGHT: ");
+        currentState = NIGHTLIGHT;
       }
       break;
     case CONNECTED:
@@ -169,20 +168,20 @@ void loop() {
           // Serial.println((char *) value);
           if (String((char) value[0]) == "N") {
             Serial.println("We Got A Message Of N Now Set Variables");
-            AM_Color = String((char*) value).substring(1);
-            // Serial.println(AM_Color);
+            PM_Color = String((char*) value).substring(2);
+            Serial.println(PM_Color);
           }
           if (String((char) value[0]) == "M") {
             Serial.println("We Got A Message Of M Now Set Variables");
-            PM_Color = String((char*) value).substring(1);
-            // Serial.println(PM_Color);
+            AM_Color = String((char*) value).substring(2);
+            // Serial.println(AM_Color);
           }
           if (String((char) value[0]) == "P" && String((char) value[1]) == "M") {
             Serial.println("We Got A Message Of PM Now Set Variables");
             PM_Time_Hour = String((char*) value).substring(2,4);
             PM_Time_Minute = String((char*) value).substring(4);
-            // Serial.println(PM_Time_Hour);
-            // Serial.println(PM_Time_Minute);
+            Serial.println(PM_Time_Hour);
+            Serial.println(PM_Time_Minute);
           }
           if (String((char) value[0]) == "A" && String((char) value[1]) == "M") {
             Serial.println("We Got A Message Of AM Now Set Variables");
@@ -199,44 +198,95 @@ void loop() {
       Serial.println(central.address());
       currentState = NOTCONNECTED;
       break;
-    case NIGHTLIGHT:
-      Serial.print("We Are in the NightLigth State");
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LED_BUILTIN, LOW);
-      break;
-    case MORNINGLIGHT:
-      Serial.print("We Are in the MorningLight State");
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LED_BUILTIN, LOW);
-      break;
-    case NIGHTALARM:
-      unsigned long currentMillis = millis();
-      if (currentMillis - previousMillis >= interval) {
-        previousMillis = currentMillis;
-        displayTime();
-      }
-      matrix.begin();
-      matrix.setBrightness(255); //set brightness to ledBrightness variable
+    case NIGHTLIGHT:{
+        if (nightStart == 0) {
+          nightStart = millis();
+          nightLightDone = false;
+          if(ledBrightness == NULL){
+            ledBrightness = 255;
+          }
+          Serial.println(PM_Color);
+          PM_Color.toCharArray(charbufPM,7);
+          rbg_NightTime = strtoul(charbufPM,0,16);
+          // Serial.print("We Are in the NightLigth State");
+          byte r =(byte)(rbg_NightTime>>16);
+          byte g =(byte)(rbg_NightTime>>8);
+          byte b =(byte)(rbg_NightTime);
 
-      //matrix test for demo
-      for (int i = 0; i < NUM_LEDS; i++) {
-        matrix.setPixelColor(i, matrix.Color(0, 100, 255));
+          Serial.println(PM_Color);
+          Serial.println(r);
+          Serial.println(g);
+          Serial.println(b);
+          for (int i = 0; i < NUM_LEDS; i++) {
+            matrix.setBrightness(ledBrightness/4);
+            matrix.setPixelColor(i, matrix.Color(r, g, b));
+          }
+          matrix.show();
+          Serial.println("Here 1");
+          delay(500);
+          for (int i = 0; i < NUM_LEDS; i++) {
+            matrix.setBrightness(ledBrightness/2);
+            matrix.setPixelColor(i, matrix.Color(r, g, b));
+          }
+          matrix.show();
+          Serial.println("Here 2");
+          delay(500);
+          for (int i = 0; i < NUM_LEDS; i++) {
+            matrix.setBrightness(ledBrightness);
+            matrix.setPixelColor(i, matrix.Color(r, g, b));
+          }
+          Serial.println("Here Last");
+          matrix.show();
+        }
+        if(!nightLightDone && millis() - nightStart >= 300000UL){
+          nightLightDone = true;
+          matrix.setBrightness(ledBrightness / 2);
+          matrix.show();
+        }
+      
+        if(AM_Time_Hour == String(now.hour()) && AM_Time_Minute == String(now.minute())){
+          currentState = MORNINGLIGHT;
+          nightStart = 0;
+        }   
       }
-      matrix.show();
       break;
-    case MORNINGALARM:
-      // Serial.print("We Are in the MorningAlarm State"); 
+    case MORNINGLIGHT:{
+      if (morningStart == 0) {
+          morningStart = millis();
+          morningLightDone = false;
+          if(ledBrightness == NULL){
+            ledBrightness = 255;
+          }
+          AM_Color.toCharArray(charbufAM,7);
+          rbg_MorningTime = strtoul(charbufAM,0,16);
+          // Serial.print("We Are in the NightLigth State");
+          byte r =(byte)(rbg_MorningTime>>16);
+          byte g =(byte)(rbg_MorningTime>>8);
+          byte b =(byte)(rbg_MorningTime);
+
+          for (int i = 0; i < NUM_LEDS; i++) {
+            matrix.setBrightness(ledBrightness);
+            matrix.setPixelColor(i, matrix.Color(r, g, b));
+          }
+          matrix.show();
+        }
+        if(!morningLightDone && millis() - morningStart >= 300000UL){
+          morningLightDone = true;
+          currentState = NOTCONNECTED; 
+        }
+      }
       break;
     default:
       break;
   }
 }
 
-//display time on LCD screen
 void displayTime() {
   now = rtc.now();
-  LCD.fillRect(40, 180, LCD.width(), 35, ILI9341_BLACK);
-  LCD.setCursor(40, 180);
+  uint16_t CREAM = LCD.color565(255,238,185);
+  LCD.setCursor(60, 200);
+  LCD.setTextSize(4);
+  LCD.setTextColor(ILI9341_BLACK, CREAM);
 
   if (now.hour() < 10) LCD.print("0");
   LCD.print(now.hour()); LCD.print(":");
@@ -246,52 +296,70 @@ void displayTime() {
   LCD.print(now.second());
 }
 
-
 // ******* TODO: FIX THIS FUNCTION SO WE CAN USE LATER IN OUR STATES *******
 // This is a function we will use later to allow for us to read in the serial
 // then make changes to our lights based on what the serial tells us
-void updateNightLightsColor(){
-  //https://forum.arduino.cc/t/code-optimising-ws2812-led-matrix-arduino/1068713
-  // put your main code here, to run repeatedly:
-  if(Serial.available() > 0)
-  {
-    int r, g, b;
-    String data = Serial.readStringUntil('\n');
+// void updateNightLightsColor(){
+//   //https://forum.arduino.cc/t/code-optimising-ws2812-led-matrix-arduino/1068713
+//   // put your main code here, to run repeatedly:
+//   if(Serial.available() > 0)
+//   {
+//     int r, g, b;
+//     String data = Serial.readStringUntil('\n');
 
-    if(sscanf(data.c_str(), "%d,%d,%d", &r, &g, &b) == 3){
-      digitalWrite(LED_BUILTIN, HIGH);
-      for (int i = 0; i < NUM_LEDS; i++) {
-        matrix.setPixelColor(i, matrix.Color(r, g, b));
-      }
-      matrix.show();
-    }
-  }
+//     if(sscanf(data.c_str(), "%d,%d,%d", &r, &g, &b) == 3){
+//       digitalWrite(LED_BUILTIN, HIGH);
+//       for (int i = 0; i < NUM_LEDS; i++) {
+//         matrix.setPixelColor(i, matrix.Color(r, g, b));
+//       }
+//       matrix.show();
+//     }
+//   }
+// }
+
+
+void snorLaxFace(){
+  uint16_t BG = LCD.color565( 30, 92, 96 );
+  uint16_t CREAM = LCD.color565(255,238,185);
+  uint16_t OUTLINE = ILI9341_BLACK; 
+  LCD.fillScreen(BG);
+
+  const int cx = 160;
+  const int cy = 120;
+  const int rectW = 300;
+  const int rectH = 200;
+
+  int x0 = cx - rectW / 2;
+  int y0 = cy - rectH / 2;
+
+  
+  LCD.fillRect(x0, y0, rectW, rectH, CREAM);
+  LCD.drawRect(x0, y0, rectW, rectH, OUTLINE);
+
+  const int notchHalf = 55;
+  const int notchDepth = 50;
+
+  int notchLeftX = cx - notchHalf;
+  int notchRightX = cx + notchHalf;
+  int notchBaseY = y0;
+  int notchApexX = cx;
+  int notchApexY = y0 + notchDepth;
+
+  LCD.fillTriangle(notchLeftX,  notchBaseY,
+                  notchRightX, notchBaseY,
+                  notchApexX,  notchApexY,
+                  BG);
+
+  LCD.drawTriangle(notchLeftX,  notchBaseY,
+                  notchRightX, notchBaseY,
+                  notchApexX,  notchApexY,
+                  BG);
+  
+  LCD.fillRect(30, 80, 80, 25, OUTLINE);
+  LCD.fillRect(190, 80, 80, 25, OUTLINE);
+  const int mouthW = 150;
+  const int mouthH = 25;
+  int x10 = 160 - mouthW / 2;
+  int y10 = 120 - mouthH / 2;
+  LCD.fillRect(x10, y10 + 45, mouthW, mouthH, OUTLINE);
 }
-
-
-
-   //https://forum.arduino.cc/t/code-optimising-ws2812-led-matrix-arduino/1068713
-
-    // put your main code here, to run repeatedly:
-    // if(Serial.available() > 0){
-    //   int r, g, b;
-    //   String data = Serial.readStringUntil('\n');
-
-    //   if(sscanf(data.c_str(), "%d,%d,%d", &r, &g, &b) == 3){
-    //     Serial.println("We did get here from Website!!!");
-    //     digitalWrite(LED_BUILTIN, HIGH);
-    //     for (int i = 0; i < NUM_PINS; i++) {
-    //       matrix.setPixelColor(i, matrix.Color(r, g, b));
-    //     }
-    //     matrix.show();
-    //   }
-    // }
-
-// ******* TODO: AT LEAST START THESE *******
-void updateDayTimeLightsColor(){}
-
-void updateTimerForNightLights(){ }
-
-void updateTimerForDayTimeLights(){ }
-
-void keepTime(){ }

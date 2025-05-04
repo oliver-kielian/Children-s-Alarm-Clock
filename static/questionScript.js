efa14524d802d4293c65218a5145dfa5e090f95c
Oliver Kielian <oliverkielian02@gmail.com>
2025-05-04 15:14:14 -0600

    rewrote question marks

--- static/questionScript.js
+++ static/questionScript.js
@@ -2,9 +2,9 @@ const colorQuestion = document.getElementById('colorQuestion');
 const  lightQuestion = document.getElementById('lightQuestion');
 
 colorQuestion.addEventListener('click', function(){
-    alert("Choose the color you want for each alarm! At the selected alarm time, the color on the alarm clock with change to the color corresponding with the selected alarm (ex: For a wake up alarm (or morning alarm) set a 7 am with the color pink. At 7 am each day the lights on the alarm clock with shine pink)");
+    alert("Customize the color for each of your alarms! When an alarm goes off, the clock will light up in the color you’ve chosen for that specific time. For example, set your 7 AM wake-up alarm to pink, and each morning at 7, your alarm clock will glow pink.");
 })
 
 lightQuestion.addEventListener('click', function(){
-    alert("Choose the time you want the lights on the alarm clock to change!")
+    alert("Set the time when you want your alarm clock to light up in your chosen color!")
 })
